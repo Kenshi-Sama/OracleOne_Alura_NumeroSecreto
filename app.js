93af96b825ea5c59b97a7d883bf036cb62e558d9
Kenshi-Sama <erikdasilvaferreira@hotmail.com>
2025-01-16 23:19:26 -0300

    Projeto inicial

---
+++ app.js
@@ -0,0 +1,24 @@
+alert('Boas vindas ao jogo do número secreto!');
+
+let numeroSecreto = 29;
+let chute;
+let tentativas = 1;
+
+
+while(chute != numeroSecreto || tentativas < 2 ){
+
+    chute = prompt('Escolha um número de 1 a 100:');
+
+    if (chute == numeroSecreto) {
+        console.log(`Parabéns, você acertou! ${numeroSecreto}`);
+    } else{
+        if(chute>numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        } else{
+            alert(`O número secreto é maior que $ama{chute}`);
+        }
+        tentativas++;
+        console.log(`Tentativa ${tentativas}`);
+    }
+    
+} 
