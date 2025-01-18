d36294375c5d6a0889e6ce8d0705ebea939cfc4c
Kenshi-Sama <erikdasilvaferreira@hotmail.com>
2025-01-18 15:11:00 -0300

    Refatorando o código do jogo

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
-alert('Boas vindas ao jogo do número secreto!');
+//Boas vindas
+alert('Bem vindo ao jogo do número secreto!');
 
-let numeroSecreto = 43;
+//declarando a dificuldade do jogo.
+
+let qtdNumeros = prompt('Escolha a dificuldade do jogo apenas declarando a quantidade de números. Intervalo de 10 números ou intervalo de 100 números?')
+
+let dificuldade = qtdNumeros == 10 ? 10 : 100;
+
+//Definindo número secreto de 1 a 100, utilizando a função Math, que inicialmente traz numeros pseudo aleatórios  entre 0 e 0,9.
+let numeroSecreto = parseInt(Math.random()*dificuldade)+1;
+console.log(numeroSecreto);
+
+//criando uma variável vazia para atribuir o valor posteriormente.
 let chute;
-let tentativas = 1;
 
+//criando um contador de tentativas.
+let qtdTentativas = 1;
 
-while(chute != numeroSecreto || tentativas < 2 ){
+/*estrutura de repetição, que deve fazer com que o jogo continue até o usuário
+acertar o número secreto.*/
+while(chute!=numeroSecreto){
 
-    chute = prompt('Escolha um número de 1 a 100:');
+    //Capturando a escolha do usuário.
+    chute = prompt(`Escolha um número de 1 a ${dificuldade}:`);
 
-    if (chute == numeroSecreto) {
-        console.log(`Parabéns, você acertou! ${numeroSecreto}`);
-    } else{
+    //Estrutura condicional do acerto, e dicas sobre o número secreto.
+    if(chute == numeroSecreto){
+        alert(`Você acertou o numero secreto: ${numeroSecreto}.`)
+        //caso o chute seja igual ao número secreto, o break para o while.
+        break;
+
+    }else{
         if(chute>numeroSecreto){
-            alert(`O número secreto é menor que ${chute}`);
-        } else{
-            alert(`O número secreto é maior que $ama{chute}`);
+            alert(`O número secreto é menor que ${chute}.`)
+        }else{
+            alert(`O número secreto é maior que ${chute}`)
         }
-        tentativas++;
-        console.log(`Tentativa ${tentativas}`);
     }
-    
-} 
+
+    //incrementando o contador de tentativas.
+    qtdTentativas++;
+}
+
+/*Utilizando operador ternário para corrigir a palavra de acordo com a qtd de tentativas.*/    
+let palavraTentativa = qtdTentativas>1?'tentativas':'tentativa';
+
+//exibindo mensagem final corretamente.
+alert(`Parabéns, você acertou o número secreto com ${qtdTentativas} ${palavraTentativa}.`)
